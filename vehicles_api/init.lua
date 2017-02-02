66d6434570b931bb2ef82d7bc070a8055a2ce2eb
Christian Danscheid <christian.danscheid@outlook.de>
2017-02-02 22:12:05 +0100

    move demo car to seperate mod

--- vehicles_api/init.lua
+++ vehicles_api/init.lua
@@ -4,14 +4,5 @@ Vehicles = {
 	path = minetest.get_modpath(minetest.get_current_modname()),
 }
 
-
-
 dofile(Vehicles.path .. "/physics.lua")
 dofile(Vehicles.path .. "/api.lua")
-
-Vehicles.register_vehicle("vehicles_api:test", {
-	mesh = "car_001.obj",
-	textures = {"textur_yellow.png"},
-	collisionbox = {-0.6, 0.0, -1.85, 1.4, 1.25, 1.25},
-	seats = 1
-})
---
+++ vehicles_demo/init.lua
@@ -0,0 +1,6 @@
+Vehicles.register_vehicle("vehicles_demo:test", {
+	mesh = "car_001.obj",
+	textures = {"textur_yellow.png"},
+	collisionbox = {-0.6, 0.0, -1.85, 1.4, 1.25, 1.25},
+	seats = 1
+})
